e4fa7b3da9c33030694a09e941eed8005e6b73f4
GiFTED <gkenny896@gmail.com>
2024-06-27 06:03:05 +0100

    Add error fallback for home page blocks table

--- app/(components)/blocks-table.tsx
+++ app/(components)/blocks-table.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import { Block } from "@/models/block";
 import { tableHeaderItems } from "./blocks-table-model";
 import BlocksTableData from "./blocks-table-data";
+import BlockError from "@/components/error";
 
 const BlocksTable = async () => {
-  const req = await fetch(process.env.URL + "/api/blocks");
-  const data: Block[] = await req.json();
+  const request = await fetch(process.env.URL + "/api/blocks");
+  const data: Block[] = await request.json();
+
+  if (request.status !== 200) {
+    return <BlockError status={request.status} message={request.statusText} />;
+  }
 
   return (
     <div className="overflow-x-auto">
